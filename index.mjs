9d05b92e4bacc9b9881b1e51fe2ba6aedc52f54a
nslebruh <nslebrun1@gmail.com>
2022-05-05 12:27:43 +1000

    index.mjs changes

--- index.mjs
+++ index.mjs
@@ -622,6 +622,7 @@ socket_app.on("connection", (socket) => {
       } else if (req.url().includes("https://lilydaleheights-vic.compass.education/Services/Calendar.svc/GetCalendarEventsByUser")) {
         if (JSON.parse(req.postData()).homePage === false) {
           let res = await req.response().json()
+          console.log(res)
           if (!res.h) {
             let data = res.d
             for (var i = 0; i < data.length; i++) {
